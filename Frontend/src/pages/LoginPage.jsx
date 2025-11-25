bd72081a66406969a15de100389f260fd5d10e85
Dev Singhal <dev.singhal@encora.com>
2025-11-25 12:43:39 +0530

    Browser router established

---
+++ Frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function LoginPage() {
+  return (
+    <div className='login-page'>
+        <div className='login-card'>
+            <h2>Welcome Back</h2>
+            <h3>Employee Management System</h3>
+            
+        </div>
+    </div>
+  )
+}
