5b853922ddcfe94d4d90c2afb47af968a2b80bff
Anup Ghimire <140532297+anoopghm@users.noreply.github.com>
2025-11-26 10:54:25 +0530

    add edit form

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import AddEditEmployee from './components/AddEditEmployee.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
