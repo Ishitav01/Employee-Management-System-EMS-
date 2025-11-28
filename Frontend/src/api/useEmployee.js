8b17fe45945ca153f0f07c3e0a9bc8bc5b991feb
Parth Jasathy <parth.jasathy@encora.com>
2025-11-28 15:19:41 +0530

    useEmployee completed

--- Frontend/src/api/useEmployee.js
+++ Frontend/src/api/useEmployee.js
@@ -3,21 +3,71 @@ import apiInterceptor from './apiInterceptor';
 
 export const useEmployee = () => {
 
-    const getAllEmployees = async () => {
-        
+    const getAllEmployeesCeo = async () => {
+    try {
+      const response = await apiInterceptor.get("http://localhost:8080/api/ceo/all-employees");
+      return response.data; // return employee list
+    } catch (error) {
+      return {success : false , data : error.response?.data || "Error fetching employees"};
+    }
+  }
+  const getAllEmployeesAdmin = async ({username}) => {
+    try {
+      const response = await apiInterceptor.get("http://localhost:8080//api/admin/employees",{
+        username 
+      });
+      return response.data; // return employee list
+    } catch (error) {
+      return {success : false , data : error.response?.data || "Error fetching employees"};
+    }
+  }
+
+    const addEmployee = async ({username,emp}) => {
+  try {
+    const response = await apiInterceptor.post(
+      "http://localhost:8080/api/admin/employees",
+      username,
+      emp
+    );
+
+    return { success: true, data: response.data||"Employee added" };
+  } catch (error) {
+    return {
+      success: false,
+      data: error.response?.data || "Error adding the employee"
+    };
+  }
+};
+
+
+    const editEmployee = async ({ username, update }) => {
+    try {
+    const response = await apiInterceptor.put(
+      "http://localhost:8080/api/admin/employees",
+      {
+        username,
+        ...update   // spread updated fields
+      }
+    );
+    return { success: true, data: response.data }
+  } catch (error) {
+    return {
+      success: false,
+      data: error.response?.data || "Error updating employee"
+    }
+  }}
+    const deleteEmployee = async (username) => {
+        try{
+            const response = apiInterceptor.delete("http://localhost:8080/api/admin/employees",{
+                username
+            })
+            return { success: true, data: response.data || "Employee deleted" };
+        }
+        catch(error){
+            return {success: false,
+      data: error.response?.data || "Error deleting the employee"}
+        }
     }
 
-    const addEmployee = async () => {
-    
-    }
-
-    const editEmployee = async () => {
-
-    }
-
-    const deleteEmployee = async () => {
-
-    }
-
-    return {addEmployee , editEmployee, deleteEmployee};
+    return {getAllEmployeesAdmin,getAllEmployeesCeo,addEmployee , editEmployee, deleteEmployee};
 }
