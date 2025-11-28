236f7c0e23707b8e7749e94402d17341a3e1062d
archit-417 <architsrivastava417@gmail.com>
2025-11-29 01:13:16 +0530

    added constraint to add only one ceo.

--- Backend/employee-management/src/main/java/com/ems/controller/AuthController.java
+++ Backend/employee-management/src/main/java/com/ems/controller/AuthController.java
@@ -37,10 +37,21 @@ public class AuthController {
 
     @PostMapping("/create-ceo")
     public ResponseEntity<?> createCEO(){
+        Map<String, String> resp = new HashMap<>();
+        resp.put("username", "ceo_user");
+        resp.put("password", "ceo123");
+
+
+        //Ensures only one CEO can be created in db
+        if(userService.existsByUsername("ceo_user")){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("CEO user already exists. "+resp.toString());
+        }
+
         userService.createCEO();
         emsService.createCEO(); 
-        
-        return ResponseEntity.status(HttpStatus.CREATED).body("CEO created./nUsername: ceo_user /nPassword: ceo123");
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(resp);
     }
 
     @PostMapping("/login")
